049ad0a0fcedf224a10b0ca53c6032250bd25c0a
Yunong <clcen512@gmail.com>
2024-07-31 18:08:41 -0300

    initial

--- fantasy/.history/server/src/routes/auth/auth.router_20240731173154.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { signUpHandler, signInHandler } from "./auth.controller";
-
-const router = Router();
-
-router.post("/signup", signUpHandler);
-router.post("/signin", signInHandler);
-
-export default router;
